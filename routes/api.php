945098997d1430672d65e0ec247e6ea22a413152
Man Mai minh <manmaiminh@Mans-Air.lan>
2025-12-18 10:39:00 +0700

    update code

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+use App\Http\Controllers\Api\Storefront\CheckoutController;
+
+Route::prefix('storefront')->group(function () {
+
+    // 🧾 Tạo đơn hàng từ checkout LIN XÉN
+    Route::post('/orders', [CheckoutController::class, 'create'])
+        ->name('api.storefront.orders.create');
+
+});
+?>
