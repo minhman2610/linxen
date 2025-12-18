fdbdc4bff8de80e6ee42ba11742a2ba3affdc53a
Man Mai minh <manmaiminh@Mans-Air.lan>
2025-12-18 10:59:42 +0700

    update code

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\Storefront\CheckoutController;
+use App\Http\Controllers\Api\CheckoutController;
+use App\Http\Controllers\Api\LocationProxyController;
 
 Route::prefix('storefront')->group(function () {
 
     // 🧾 Tạo đơn hàng từ checkout LIN XÉN
     Route::post('/orders', [CheckoutController::class, 'create'])
         ->name('api.storefront.orders.create');
+
+    // 📍 Khu vực (proxy từ ERP)
+    Route::get('/locations', [LocationProxyController::class, 'locations'])
+        ->name('api.storefront.locations');
+
+    // 🏠 Phường / xã theo khu vực
+    Route::get('/locations/{locationId}/wards', [LocationProxyController::class, 'wards'])
+        ->name('api.storefront.wards');
 
 });
