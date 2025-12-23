a017abcfd5f691d4d6a26eca855d0526cac90fb7
Man Mai minh <manmaiminh@Mans-MacBook-Air.local>
2025-12-23 08:16:04 +0700

    linxen update

--- resources/views/storefront/luxe/layouts/app.blade.php
+++ resources/views/storefront/luxe/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ $brand ?? 'LUXE' }} — 3MG Storefront</title>
+    <title>{{ $brand ?? 'LIN XÉN' }} — ĐAM MÊ VÁY</title>
 
     {{-- ============================
         BASE CSS
