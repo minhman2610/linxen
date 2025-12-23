075c1a28a8138d1e8bfc98590ce31464d2283b4d
Man Mai minh <manmaiminh@Mans-Air.lan>
2025-12-23 10:45:51 +0700

    linxen update

--- resources/views/storefront/luxe/components/bottom-nav.blade.php
+++ resources/views/storefront/luxe/components/bottom-nav.blade.php
@@ -4,14 +4,14 @@
     <a href="{{ route('linxen.home') }}"
        class="lx-nav-item {{ request()->routeIs('linxen.home') ? 'active' : '' }}">
         <img src="/themes/luxe/assets/icons/icon-home.svg" class="icon">
-        <span>Trang chủ</span>
+        <span class="lx-nav-text">Trang chủ</span>
     </a>
 
     {{-- SEARCH --}}
     <a href="/search"
        class="lx-nav-item {{ request()->is('search') ? 'active' : '' }}">
         <img src="/themes/luxe/assets/icons/icon-search.svg" class="icon">
-        <span>Tìm kiếm</span>
+        <span class="lx-nav-text">Tìm kiếm</span>
     </a>
 
     {{-- REELS --}}
@@ -20,21 +20,21 @@
         <span class="lx-reels-bg">
             <img src="/themes/luxe/assets/icons/icon-reels.svg" class="icon">
         </span>
-        <span>Reels</span>
+        <span class="lx-nav-text">Reels</span>
     </a>
 
     {{-- WISHLIST --}}
     <a href="/wishlist"
        class="lx-nav-item {{ request()->is('wishlist') ? 'active' : '' }}">
         <img src="/themes/luxe/assets/icons/icon-heart.svg" class="icon">
-        <span>Yêu thích</span>
+        <span class="lx-nav-text">Yêu thích</span>
     </a>
 
     {{-- CART --}}
     <a href="{{ route('linxen.cart') }}"
        class="lx-nav-item cart-btn {{ request()->routeIs('linxen.cart') ? 'active' : '' }}">
         <img src="/themes/luxe/assets/icons/icon-cart.svg" class="icon">
-        <span>Giỏ hàng</span>
+        <span class="lx-nav-text">Giỏ hàng</span>
 
         <span class="cart-count" id="lxCartCount">
             {{ array_sum(array_column(session('cart', []), 'qty')) }}
