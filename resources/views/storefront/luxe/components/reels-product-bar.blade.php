c649bfb187a7b9c3662d822aa5c64b6b17de7ca6
Man Mai minh <manmaiminh@Mans-MacBook-Air.local>
2025-12-24 10:37:45 +0700

    linxen update

--- resources/views/storefront/luxe/components/reels-product-bar.blade.php
+++ resources/views/storefront/luxe/components/reels-product-bar.blade.php
@@ -1,12 +1,28 @@
 <div class="lx-reels-product-bar">
 
-    <div class="lx-reels-product-info">
-        <strong id="lxReelsName">Tên sản phẩm</strong>
-        <span id="lxReelsPrice">0₫</span>
+    {{-- THUMBNAIL --}}
+    <div class="lx-reels-thumb">
+        <img
+            id="lxReelsThumb"
+            src="/images/no-image.png"
+            alt="Product thumbnail">
     </div>
 
-    <button class="lx-reels-add-cart" id="lxReelsAddCart">
-        Thêm vào giỏ
+    {{-- INFO --}}
+    <div class="lx-reels-product-info">
+        <strong id="lxReelsName">Tên sản phẩm</strong>
+
+        <div class="lx-reels-meta">
+            <span class="lx-reels-price" id="lxReelsPrice">0₫</span>
+            <span class="lx-reels-tag">Có sẵn</span>
+        </div>
+    </div>
+
+    {{-- ACTION --}}
+    <button
+        class="lx-reels-add-cart"
+        id="lxReelsAddCart">
+        + Giỏ hàng
     </button>
 
 </div>
