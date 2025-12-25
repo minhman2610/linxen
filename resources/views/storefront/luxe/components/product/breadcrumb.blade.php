36c25f584d8f3a3dc0fa55c458b81af31b973d40
Man Mai minh <manmaiminh@Mans-MacBook-Air.local>
2025-12-25 10:09:14 +0700

    linxen update

--- resources/views/storefront/luxe/components/product/breadcrumb.blade.php
+++ resources/views/storefront/luxe/components/product/breadcrumb.blade.php
@@ -1,21 +1,13 @@
-{{-- =====================================================
-   PRODUCT BREADCRUMB – COUTURE FRAMES
-===================================================== --}}
 @if(!empty($breadcrumbs))
     <nav class="lx-couture-breadcrumb" aria-label="breadcrumb">
 
-        {{-- HOME --}}
         <span class="lx-couture-item home">
             <span class="lx-couture-label">Trang chủ</span>
         </span>
 
         @foreach($breadcrumbs as $crumb)
-            {{-- CONNECTOR --}}
-            <span class="lx-couture-connector">
-                ✦
-            </span>
+            <span class="lx-couture-connector">✦</span>
 
-            {{-- ITEM --}}
             <span class="lx-couture-item {{ empty($crumb['url']) ? 'active' : '' }}">
                 <span class="lx-couture-label">
                     {{ $crumb['name'] }}
