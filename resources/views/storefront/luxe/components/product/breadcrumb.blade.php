864cc4d5a2a9847e5f3cf9b6ce4b2e8245775f53
Man Mai minh <manmaiminh@Mans-MacBook-Air.local>
2025-12-25 10:02:33 +0700

    linxen update

--- resources/views/storefront/luxe/components/product/breadcrumb.blade.php
+++ resources/views/storefront/luxe/components/product/breadcrumb.blade.php
@@ -1,22 +1,22 @@
 {{-- =====================================================
-   PRODUCT CATEGORY TABS (BREADCRUMB)
+   PRODUCT CATEGORY PATH – CONNECTED
 ===================================================== --}}
-<nav class="lx-category-tabs" aria-label="breadcrumb">
+<nav class="lx-cat-path" aria-label="breadcrumb">
 
-    {{-- HOME TAB --}}
-    <a href="/" class="lx-cat-tab">
+    {{-- HOME --}}
+    <a href="/" class="lx-cat-node">
         <span class="lx-cat-icon">🏠</span>
         <span class="lx-cat-text">Trang chủ</span>
     </a>
 
     @foreach($breadcrumbs as $crumb)
         @if(!empty($crumb['url']))
-            <a href="{{ $crumb['url'] }}" class="lx-cat-tab">
+            <a href="{{ $crumb['url'] }}" class="lx-cat-node">
                 <span class="lx-cat-icon">🏷️</span>
                 <span class="lx-cat-text">{{ $crumb['name'] }}</span>
             </a>
         @else
-            <span class="lx-cat-tab active">
+            <span class="lx-cat-node active">
                 <span class="lx-cat-icon">👗</span>
                 <span class="lx-cat-text">{{ $crumb['name'] }}</span>
             </span>
