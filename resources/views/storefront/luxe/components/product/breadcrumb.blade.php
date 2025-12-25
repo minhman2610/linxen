e21a27240ca330956c80ef4719dffff90931444d
Man Mai minh <manmaiminh@Mans-MacBook-Air.local>
2025-12-25 10:46:02 +0700

    linxen update

--- resources/views/storefront/luxe/components/product/breadcrumb.blade.php
+++ resources/views/storefront/luxe/components/product/breadcrumb.blade.php
@@ -1,27 +1,36 @@
 @if(!empty($breadcrumbs))
 <nav class="lx-art-breadcrumb" aria-label="breadcrumb">
-    @foreach($breadcrumbs as $index => $crumb)
 
-        @if($index === 0)
-            <a href="{{ $crumb['url'] ?? '/' }}"
-               class="lx-art-item is-home">
-                {{ $crumb['name'] }}
+    {{-- HOME --}}
+    <a href="/"
+       class="lx-art-item is-home"
+       title="Trang chủ">
+        <span class="lx-art-icon">⌂</span>
+        <span class="lx-art-label">Trang chủ</span>
+    </a>
+
+    {{-- OTHER CRUMBS --}}
+    @foreach($breadcrumbs as $crumb)
+
+        <span class="lx-art-connector" aria-hidden="true">⟡</span>
+
+        @if(!empty($crumb['url']))
+            <a href="{{ $crumb['url'] }}"
+               class="lx-art-item"
+               title="{{ $crumb['name'] }}">
+                <span class="lx-art-label">
+                    {{ Str::upper($crumb['name']) }}
+                </span>
             </a>
         @else
-            <span class="lx-art-connector">⟡</span>
-
-            @if(!empty($crumb['url']))
-                <a href="{{ $crumb['url'] }}"
-                   class="lx-art-item">
-                    {{ $crumb['name'] }}
-                </a>
-            @else
-                <span class="lx-art-item is-active">
-                    {{ $crumb['name'] }}
+            <span class="lx-art-item is-active">
+                <span class="lx-art-label">
+                    {{ Str::upper($crumb['name']) }}
                 </span>
-            @endif
+            </span>
         @endif
 
     @endforeach
+
 </nav>
 @endif
