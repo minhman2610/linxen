3ec67861bf82c395155bd30137840b53a601722b
Man Mai minh <manmaiminh@Mans-MacBook-Air.local>
2025-12-25 15:34:21 +0700

    linxen update

--- resources/views/storefront/luxe/components/header.blade.php
+++ resources/views/storefront/luxe/components/header.blade.php
@@ -38,7 +38,17 @@
 </span>
 
 </a>
+{{-- CART --}}
+<a href="{{ route('linxen.cart') }}"
+   class="lx-header-btn cart-btn"
+   aria-label="Cart">
 
+    <img src="{{ asset('themes/luxe/assets/icons/icon-cart.svg') }}" alt="Cart">
+    <span class="cart-count js-cart-count">
+    {{ array_sum(array_column(session('cart', []), 'qty')) }}
+</span>
+
+</a>
 
 </header>
 
