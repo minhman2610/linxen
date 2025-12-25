367b2526f95a245c1ffce07605901e0a9ae7efde
Man Mai minh <manmaiminh@Mans-MacBook-Air.local>
2025-12-25 22:48:15 +0700

    linxen update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+
+        // 🔥 FIX CSRF CHO API STOREFRONT
+        $middleware->validateCsrfTokens(except: [
+            'api/storefront/*',
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
