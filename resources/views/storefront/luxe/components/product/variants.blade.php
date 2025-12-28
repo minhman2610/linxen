c72f386519316dd6e4d5c56498cab70761733a61
Man Mai minh <manmaiminh@Mans-MacBook-Air.local>
2025-12-29 00:29:14 +0700

    linxen update

--- resources/views/storefront/luxe/components/product/variants.blade.php
+++ resources/views/storefront/luxe/components/product/variants.blade.php
@@ -2,7 +2,10 @@
 <div class="lx-product-variants" id="lxVariants">
 
     @foreach($attributes as $attr => $values)
-        @php $attrKey = Str::slug($attr, '_'); @endphp
+        @php
+            $attrKey   = Str::slug($attr, '_');
+            $isSizeAttr = Str::lower($attr) === 'size';
+        @endphp
 
         <div class="lx-variant-row"
              data-attr="{{ $attr }}"
@@ -14,14 +17,32 @@
 
             <div class="lx-variant-options">
                 @foreach($values as $val)
-                    <button
-                        type="button"
-                        class="variant-option"
-                        data-attr="{{ $attr }}"
-                        data-attr-key="{{ $attrKey }}"
-                        data-value="{{ $val }}">
-                        {{ $val }}
-                    </button>
+
+                    @if($isSizeAttr && is_array($val))
+                        {{-- 🔑 SIZE VARIANT – SKU CON --}}
+                        <button
+                            type="button"
+                            class="variant-option"
+                            data-attr="{{ $attr }}"
+                            data-attr-key="{{ $attrKey }}"
+                            data-value="{{ $val['label'] }}"
+                            data-product-id="{{ $val['product_id'] }}"
+                        >
+                            {{ $val['label'] }}
+                        </button>
+                    @else
+                        {{-- OTHER VARIANTS (COLOR, MATERIAL...) --}}
+                        <button
+                            type="button"
+                            class="variant-option"
+                            data-attr="{{ $attr }}"
+                            data-attr-key="{{ $attrKey }}"
+                            data-value="{{ is_array($val) ? ($val['label'] ?? '') : $val }}"
+                        >
+                            {{ is_array($val) ? ($val['label'] ?? '') : $val }}
+                        </button>
+                    @endif
+
                 @endforeach
             </div>
 
