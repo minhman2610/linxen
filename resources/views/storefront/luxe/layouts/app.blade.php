a0cc9152f3a32b29678f822f2dc178d309712d0e
Man Mai minh <manmaiminh@Mans-MacBook-Air.local>
2026-02-01 11:26:22 +0700

    linxen update

--- resources/views/storefront/luxe/layouts/app.blade.php
+++ resources/views/storefront/luxe/layouts/app.blade.php
@@ -11,11 +11,13 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1583669999592387');
+fbq('init', '1138713574876626');
 fbq('track', 'PageView');
 </script>
+
+
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1583669999592387&ev=PageView&noscript=1"
+src="https://www.facebook.com/tr?id=1138713574876626&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Meta Pixel Code -->
     {{-- =====================================================
