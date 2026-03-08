229b286f58a7fd0fe1bd41aef1b018f77bd68c1f
Man Mai minh <manmaiminh@Mans-MacBook-Air.local>
2026-03-08 15:08:28 +0700

    linxen update

--- resources/views/storefront/luxe/components/mobile-menu.blade.php
+++ resources/views/storefront/luxe/components/mobile-menu.blade.php
@@ -4,74 +4,98 @@
 {{-- MOBILE MENU --}}
 <div class="lx-mobile-menu" id="lxMobileMenu" aria-hidden="true">
 
-    <!-- Brand Highlight Zone -->
+    {{-- BRAND ZONE --}}
     <div class="lx-menu-brand-zone">
+
         <div class="lx-brand-logo">LIN XÉN</div>
         <div class="lx-brand-tagline">Thời trang cao cấp</div>
 
         <button class="close-btn" id="lxMenuClose" type="button">✕</button>
+
     </div>
 
-    <!-- Menu list -->
+
     <ul class="lx-menu-list">
 
-        {{-- ✅ HOME --}}
-        <li class="lx-menu-item">
-            <a href="{{ route('linxen.home') }}" class="lx-menu-link">
-                <img src="/themes/luxe/assets/icons/icon-home.svg" class="menu-icon" alt="">
-                <span>Trang chủ</span>
+        {{-- 🔥 FLASH SALE --}}
+        <li class="lx-menu-item highlight-sale">
+            <a href="/collections/sale" class="lx-menu-link">
+                <img src="/themes/luxe/assets/icons/icon-fire.svg" class="menu-icon" alt="">
+                <span>SALE 30-50%</span>
             </a>
         </li>
 
-        {{-- 🟡 CATEGORIES (DEMO) --}}
+
+        {{-- ⚡ BEST SELLER --}}
+        <li class="lx-menu-item">
+            <a href="/collections/best-seller" class="lx-menu-link">
+                <img src="/themes/luxe/assets/icons/icon-star.svg" class="menu-icon" alt="">
+                <span>Bán chạy</span>
+            </a>
+        </li>
+
+
+        {{-- 🆕 NEW ARRIVAL --}}
+        <li class="lx-menu-item">
+            <a href="/collections/new" class="lx-menu-link">
+                <img src="/themes/luxe/assets/icons/icon-new.svg" class="menu-icon" alt="">
+                <span>Hàng mới</span>
+            </a>
+        </li>
+
+
+        {{-- CATEGORIES --}}
         <li class="lx-menu-item has-sub">
+
             <button type="button" class="lx-menu-link lx-menu-toggle">
+
                 <img src="/themes/luxe/assets/icons/icon-categories.svg" class="menu-icon" alt="">
                 <span>Danh mục</span>
+
             </button>
+
             <ul class="lx-submenu">
-                {{-- DEMO – cần map slug thật --}}
-                <li><a href="#">Váy</a></li>
-                <li><a href="#">Áo</a></li>
-                <li><a href="#">Quần</a></li>
-                <li><a href="#">Hàng mới</a></li>
-                <li><a href="#">Khuyến mãi</a></li>
+
+                <li>
+                    <a href="/collections/vay">Váy</a>
+                </li>
+
+                <li>
+                    <a href="/collections/ao">Áo</a>
+                </li>
+
+                <li>
+                    <a href="/collections/quan">Quần</a>
+                </li>
+
+                <li>
+                    <a href="/collections/set">Set</a>
+                </li>
+
             </ul>
+
         </li>
 
-        {{-- 🟡 NEW ARRIVALS (DEMO – cần collection slug thật) --}}
-        <li class="lx-menu-item">
-            <a href="#" class="lx-menu-link">
-                <img src="/themes/luxe/assets/icons/icon-star.svg" class="menu-icon" alt="">
-                <span>Hàng mới về</span>
-            </a>
-        </li>
 
-        {{-- 🟡 HIGH FASHION COLLECTION (DEMO) --}}
+        {{-- COLLECTION --}}
         <li class="lx-menu-item">
-            <a href="#" class="lx-menu-link">
-                <img src="/themes/luxe/assets/icons/icon-fire.svg" class="menu-icon" alt="">
+            <a href="/collections/high-fashion" class="lx-menu-link">
+                <img src="/themes/luxe/assets/icons/icon-diamond.svg" class="menu-icon" alt="">
                 <span>Bộ sưu tập cao cấp</span>
             </a>
         </li>
 
-        {{-- 🟡 WISHLIST (CHƯA CÓ) --}}
+
+        {{-- WISHLIST --}}
         <li class="lx-menu-item">
-            <a href="#" class="lx-menu-link">
+            <a href="/wishlist" class="lx-menu-link">
                 <img src="/themes/luxe/assets/icons/icon-heart.svg" class="menu-icon" alt="">
                 <span>Yêu thích</span>
             </a>
         </li>
 
-        {{-- 🟡 SUPPORT (CHƯA CÓ) --}}
-        <li class="lx-menu-item">
-            <a href="#" class="lx-menu-link">
-                <img src="/themes/luxe/assets/icons/icon-info.svg" class="menu-icon" alt="">
-                <span>Trợ giúp</span>
-            </a>
-        </li>
 
-        {{-- ✅ ACCOUNT --}}
+        {{-- ACCOUNT --}}
         <li class="lx-menu-item">
             <a href="{{ route('linxen.account.index') }}" class="lx-menu-link">
                 <img src="/themes/luxe/assets/icons/icon-account.svg" class="menu-icon" alt="">
@@ -79,11 +103,21 @@
             </a>
         </li>
 
-        {{-- ✅ ORDERS --}}
+
+        {{-- ORDERS --}}
         <li class="lx-menu-item">
             <a href="{{ route('linxen.account.orders') }}" class="lx-menu-link">
                 <img src="/themes/luxe/assets/icons/icon-order.svg" class="menu-icon" alt="">
                 <span>Đơn hàng</span>
+            </a>
+        </li>
+
+
+        {{-- SUPPORT --}}
+        <li class="lx-menu-item">
+            <a href="/support" class="lx-menu-link">
+                <img src="/themes/luxe/assets/icons/icon-info.svg" class="menu-icon" alt="">
+                <span>Trợ giúp</span>
             </a>
         </li>
 
